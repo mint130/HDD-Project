2590dcd1371cab66866c6b4d46cad2ec019ad98e
mint130 <mintminju130@naver.com>
2023-05-15 16:34:08 +0900

    05-15 frontend sign up page

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, {useEffect}from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
+import axios from "axios";
+import SignUp from './components/Signup';
+import SignIn from './components/Signin';
+import Main from './components/Main';
 
-
-const App = ()=>{
+function LandingPage() {
+    useEffect(() => {
+        axios.get('/api/hello')
+            .then(response => console.log(response.data))
+    }, [])
     return (
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/home" element={<Home/>}/>
-                    <Route path="/about" element={<About/>}/>
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+       <div className='App'>
+          <BrowserRouter>
+
+            <Routes>
+                <Route path="/" element={<Main/>}></Route>
+                <Route path="/api/auth/signin" element={<SignIn/>}></Route>
+                <Route path="/api/auth/signup/create" element={<SignUp/>}></Route>
+            </Routes>
+          </BrowserRouter>
+       </div>
+    )
 }
 
-export default App;
+export default LandingPage;
---
+++ src/main/frontend/src/components/Main.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import SignIn from "./Signin";
+
+function Main() {
+
+}
+
+export default Main;
