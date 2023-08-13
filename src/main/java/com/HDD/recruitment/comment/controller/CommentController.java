7d50132185bc4eacf700f26ba1e4b3ac6166a5d5
ooing-0720 <ooing-0720@naver.com>
2023-08-13 22:05:53 +0900

    08-13 구인글 댓글 수정(리팩토링 필요)

--- src/main/java/com/HDD/recruitment/comment/controller/CommentController.java
+++ src/main/java/com/HDD/recruitment/comment/controller/CommentController.java
@@ -1,50 +1,42 @@
-package com.HDD.recruitment.comment.controller;
-
-import com.HDD.management.webDto.MessageResponse;
-import com.HDD.recruitment.comment.model.Comment;
-import com.HDD.recruitment.comment.service.CommentService;
-import com.HDD.recruitment.comment.webDto.CommentRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@CrossOrigin(origins = "*", maxAge = 3600)
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/{boardId}")
-@PreAuthorize("hasRole('MEMBER')")
-public class CommentController {
-
-    private final CommentService commentService;
-
-    @PostMapping("/comment")
-    public ResponseEntity<?> writeComment(@AuthenticationPrincipal UserDetails userDetails, @PathVariable String boardId, @RequestBody CommentRequest request) throws Exception {
-        Comment comment = new Comment(userDetails.getUsername(), boardId, request);
-        commentService.insertComment(boardId, null, comment);
-        return ResponseEntity.ok(new MessageResponse("댓글이 등록되었습니다"));
-    }
-
-    @PostMapping("/comment/{commentId}")
-    public ResponseEntity<?> writeReply(@AuthenticationPrincipal UserDetails userDetails, @PathVariable String boardId, @PathVariable String commentId, @RequestBody CommentRequest request) throws Exception {
-        Comment comment = new Comment(userDetails.getUsername(), boardId, request);
-        commentService.insertComment(boardId, commentId, comment);
-        return ResponseEntity.ok(new MessageResponse("댓글이 등록되었습니다"));
-    }
-
-    @GetMapping()
-    public ResponseEntity<?> getComments(@PathVariable String boardId) throws Exception {
-        List<Comment> commentList = commentService.getComments(boardId);
-        return ResponseEntity.ok(commentList);
-    }
-
-    @GetMapping("/{commentId}/delete")
-    public ResponseEntity<?> deleteComment(@PathVariable String boardId, @PathVariable String commentId) throws Exception {
-        commentService.deleteComment(boardId, commentId);
-        return ResponseEntity.ok(new MessageResponse("댓글이 삭제되었습니다"));
-    }
-}
+//package com.HDD.recruitment.comment.controller;
+//
+//import com.HDD.management.webDto.MessageResponse;
+//import com.HDD.recruitment.comment.model.Comment;
+//import com.HDD.recruitment.comment.service.CommentService;
+//import com.HDD.recruitment.comment.webDto.CommentRequest;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.core.annotation.AuthenticationPrincipal;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RequestMapping("/{boardId}")
+//@RestController
+//public abstract class CommentController {
+//
+//    protected CommentService commentService;
+//
+//    @PostMapping("/comment")
+//    public ResponseEntity<?> writeComment(@AuthenticationPrincipal UserDetails userDetails, @PathVariable String boardId, @RequestBody CommentRequest request) throws Exception {
+//        Comment comment = new Comment(userDetails.getUsername(), boardId, request);
+//        commentService.insertComment(boardId, null, comment);
+//        return ResponseEntity.ok(new MessageResponse("댓글이 등록되었습니다"));
+//    }
+//
+//    @PostMapping("/comment/{commentId}")
+//    public ResponseEntity<?> writeReply(@AuthenticationPrincipal UserDetails userDetails, @PathVariable String boardId, @PathVariable String commentId, @RequestBody CommentRequest request) throws Exception {
+//        Comment comment = new Comment(userDetails.getUsername(), boardId, request);
+//        commentService.insertComment(boardId, commentId, comment);
+//        return ResponseEntity.ok(new MessageResponse("댓글이 등록되었습니다"));
+//    }
+//
+//    @GetMapping("/{commentId}/cdelete")
+//    public ResponseEntity<?> deleteComment(@PathVariable String boardId, @PathVariable String commentId) throws Exception {
+//        commentService.deleteComment(boardId, commentId);
+//        return ResponseEntity.ok(new MessageResponse("댓글이 삭제되었습니다"));
+//    }
+//}
--- src/main/java/com/HDD/recruitment/comment/controller/PJBoardCommentController.java
+++ src/main/java/com/HDD/recruitment/comment/controller/PJBoardCommentController.java
@@ -1,13 +1,51 @@
 package com.HDD.recruitment.comment.controller;
 
+import com.HDD.management.webDto.MessageResponse;
+import com.HDD.recruitment.comment.model.Comment;
 import com.HDD.recruitment.comment.service.CommentService;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.HDD.recruitment.comment.webDto.CommentRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/recruitment/project")
-public class PJBoardCommentController extends CommentController {
+import javax.annotation.PostConstruct;
+import java.util.List;
 
-    public PJBoardCommentController(CommentService commentService) {
-        super(commentService);
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/recruitment/project/{boardId}")
+@PreAuthorize("hasRole('MEMBER')")
+public class PJBoardCommentController{
+
+    private final CommentService commentService;
+
+    @PostConstruct
+    public void init(){
         commentService.setCollectionName("ProjectBoard");
+    }
+
+    @PostMapping("/comment")
+    public ResponseEntity<?> writeComment(@AuthenticationPrincipal UserDetails userDetails, @PathVariable String boardId, @RequestBody CommentRequest request) throws Exception {
+        Comment comment = new Comment(userDetails.getUsername(), boardId, request);
+        commentService.insertComment(boardId, null, comment);
+        return ResponseEntity.ok(new MessageResponse("댓글이 등록되었습니다"));
+    }
+
+    @PostMapping("/comment/{commentId}")
+    public ResponseEntity<?> writeReply(@AuthenticationPrincipal UserDetails userDetails, @PathVariable String boardId, @PathVariable String commentId, @RequestBody CommentRequest request) throws Exception {
+        Comment comment = new Comment(userDetails.getUsername(), boardId, request);
+        commentService.insertComment(boardId, commentId, comment);
+        return ResponseEntity.ok(new MessageResponse("댓글이 등록되었습니다"));
+    }
+
+    @GetMapping("/{commentId}/delete")
+    public ResponseEntity<?> deleteComment(@PathVariable String boardId, @PathVariable String commentId) throws Exception {
+        commentService.deleteComment(boardId, commentId);
+        return ResponseEntity.ok(new MessageResponse("댓글이 삭제되었습니다"));
     }
 }
