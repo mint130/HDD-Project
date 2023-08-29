4bc7e2587fbb0ccd67162d84c71ae8bff4de9018
mint130 <mintminju130@naver.com>
2023-08-30 01:12:03 +0900

    08-30 검색 및 기타 CSS 수정

--- src/main/frontend/src/components/recruit/search/Roommate_recruit_search.js
+++ src/main/frontend/src/components/recruit/search/Roommate_recruit_search.js
@@ -39,36 +39,46 @@ function Roommate_recruit_search({ onSearch }) {
 
     return (
         <div className={styles.search}>
-            <dl className={styles.row}>
-                <dt className={styles.item}>성별</dt>
-                <dd>
-                    <select className={styles.select} name="sex" value={searchParams.sex} onChange={(e) => handleInputChange("sex", e.target.value)}>
-                        <option value="M">남성</option>
-                        <option value="F">여성</option>
-                    </select>
-                </dd>
-            </dl>
-            <dl className={styles.row}>
-                <label>흡연 여부</label>
-                <input
-                    type="checkbox"
-                    name="smoke"
-                    checked={searchParams.smoke}
-                    onChange={(e) => handleInputChange("smoke", e.target.checked)}
-                />
-            </dl>
-            <dl className={styles.row}>
-                <label>유형</label>
-                <select className={styles.select} name="dormType" value={searchParams.dormType} onChange={(e)=>handleInputChange("dormType", parseInt(e.target.value))}>
-                    <option value="">선택 안 함</option>
-                    <option value={1}>1기숙사</option>
-                    <option value={2}>2기숙사</option>
-                    <option value={3}>3기숙사</option>
-                    <option value={0}>자취</option>
-                </select>
-            </dl>
+            <div className={styles.search_area}>
+                <dl className={styles.column}>
+                    <dt className={styles.item}>성별</dt>
+                    <dd className={styles.content}>
+                        <select className={styles.select} name="sex" value={searchParams.sex} onChange={(e) => handleInputChange("sex", e.target.value)}>
+                            <option value="M">남성</option>
+                            <option value="F">여성</option>
+                        </select>
+                    </dd>
+                </dl>
+                <dl className={styles.column}>
+                    <dt className={styles.item}>흡연 여부</dt>
+                    <dd className={styles.content}>
+                        <input
+                            type="checkbox"
+                            name="smoke"
+                            className={styles.checkbox}
+                            checked={searchParams.smoke}
+                            onChange={(e) => handleInputChange("smoke", e.target.checked)}
+                        />
+                    </dd>
+                </dl>
+                <dl className={styles.column}>
+                    <dt className={styles.item}>유형</dt>
+                    <dd className={styles.content}>
+                        <select className={styles.select} name="dormType" value={searchParams.dormType} onChange={(e)=>handleInputChange("dormType", parseInt(e.target.value))}>
+                            <option value="">선택 안 함</option>
+                            <option value={1}>1기숙사</option>
+                            <option value={2}>2기숙사</option>
+                            <option value={3}>3기숙사</option>
+                            <option value={0}>자취</option>
+                        </select>
+                    </dd>
 
-            <button onClick={handleSearch}>검색</button>
+                </dl>
+            </div>
+            <div className={styles.btn_area}>
+                <button className={styles.btn_type + " " + styles.btn_secondary} onClick={handleSearch}>검색</button>
+            </div>
+
         </div>
     );
 }
