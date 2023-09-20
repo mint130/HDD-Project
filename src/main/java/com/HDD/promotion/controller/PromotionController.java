860c71e987dc8b8c19bb869c2b824e072ae92bdd
ooing-0720 <ooing-0720@naver.com>
2023-09-20 22:58:25 +0900

    09-20 홍보 게시판 기능

---
+++ src/main/java/com/HDD/promotion/controller/PromotionController.java
@@ -0,0 +1,41 @@
+package com.HDD.promotion.controller;
+
+import com.HDD.management.webDto.MessageResponse;
+import com.HDD.promotion.model.Promotion;
+import com.HDD.promotion.service.PromotionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/promotion")
+@PreAuthorize("hasRole('MEMBER')")
+public class PromotionController {
+
+    private final PromotionService promotionService;
+
+    @GetMapping()
+    public ResponseEntity<?> getPromotions(@RequestParam(value = "hall", required = false) String hall) throws Exception {
+        List<Promotion> promotionList;
+        if (hall == null) {
+            promotionList = promotionService.getAllPromotions();
+        } else {
+            promotionList = promotionService.getPromotions(hall);
+        }
+        return ResponseEntity.ok(promotionList);
+    }
+
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addPromotion(@RequestBody Promotion promotion) throws Exception {
+        promotionService.insertPromotion(promotion);
+        return ResponseEntity.ok(new MessageResponse("추가되었습니다"));
+    }
+
+
+}
