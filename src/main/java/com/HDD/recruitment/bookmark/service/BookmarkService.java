95177cdfa5cd429da757227faa673addeef8b6be
ooing-0720 <ooing-0720@naver.com>
2023-10-08 16:25:17 +0900

    10-08 구인게시판 북마크 추가

---
+++ src/main/java/com/HDD/recruitment/bookmark/service/BookmarkService.java
@@ -0,0 +1,170 @@
+package com.HDD.recruitment.bookmark.service;
+
+import com.HDD.recruitment.bookmark.model.Bookmark;
+import com.HDD.recruitment.model.ProjectBoard;
+import com.HDD.recruitment.model.RoommateBoard;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+import com.google.firebase.cloud.FirestoreClient;
+import org.checkerframework.checker.units.qual.C;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class BookmarkService {
+    private static final String COLLECTION_NAME = "Bookmark";
+    private final Firestore firestore = FirestoreClient.getFirestore();
+
+    // 프로젝트 북마크 추가
+    public String addProjectBookmark(String memberId, String boardId) throws Exception{
+        DocumentReference documentReference
+                = firestore.collection(COLLECTION_NAME).document(memberId);
+        ApiFuture<DocumentSnapshot> apiFuture = documentReference.get();
+        DocumentSnapshot snapshot = apiFuture.get();
+        Bookmark bookmark = null;
+
+        if(snapshot.exists()) {
+            bookmark = snapshot.toObject(Bookmark.class);
+            if (bookmark.getProjectBoardId() == null) {
+                bookmark.setProjectBoardId(Arrays.asList(boardId));
+            } else {
+                bookmark.addProjectBookmark(boardId);
+            }
+            documentReference.set(bookmark);
+            return "update";
+        } else {
+            bookmark = new Bookmark();
+            bookmark.setMemberId(memberId);
+            bookmark.setProjectBoardId(Arrays.asList(boardId));
+            documentReference.set(bookmark);
+            return "add";
+        }
+    }
+
+    // 룸메이트 북마크 추가
+    public String addRoommateBookmark(String memberId, String boardId) throws Exception {
+        DocumentReference documentReference
+                = firestore.collection(COLLECTION_NAME).document(memberId);
+        ApiFuture<DocumentSnapshot> apiFuture = documentReference.get();
+        DocumentSnapshot snapshot = apiFuture.get();
+        Bookmark bookmark = null;
+
+        if(snapshot.exists()) {
+            bookmark = snapshot.toObject(Bookmark.class);
+            if(bookmark.getRoommateBoardId() == null) {
+                bookmark.setRoommateBoardId(Arrays.asList(boardId));
+            }
+            else {
+                bookmark.addRoommateBookmark(boardId);
+            }
+            documentReference.set(bookmark);
+            return "update";
+        } else {
+            bookmark = new Bookmark();
+            bookmark.setMemberId(memberId);
+            bookmark.setRoommateBoardId(Arrays.asList(boardId));
+            documentReference.set(bookmark);
+            return "add";
+        }
+    }
+
+    // 프로젝트 북마크 가져오기
+    public List<ProjectBoard> getProjectBookmarks(String memberId) throws Exception{
+        List<ProjectBoard> list = new ArrayList<>();
+        CollectionReference collectionReference = firestore.collection(COLLECTION_NAME);
+        ApiFuture<DocumentSnapshot> apiFuture
+                = collectionReference.document(memberId).get();
+        DocumentSnapshot snapshot = apiFuture.get();
+        Bookmark bookmark = null;
+
+        if (snapshot.exists()) {
+            bookmark = snapshot.toObject(Bookmark.class);
+            List<String> boardId = bookmark.getProjectBoardId();
+
+            for(String id : boardId) {
+                ApiFuture<DocumentSnapshot> boardApi
+                        = collectionReference.document(id).get();
+                DocumentSnapshot documentSnapshot = boardApi.get();
+                list.add(documentSnapshot.toObject(ProjectBoard.class));
+            }
+            list.sort(Comparator.comparing(ProjectBoard::getCreated).reversed());
+            return list;
+        } else return null;
+    }
+
+    public List<RoommateBoard> getRoommateBookmarks(String memberId) throws Exception{
+        List<RoommateBoard> list = new ArrayList<>();
+        CollectionReference collectionReference = firestore.collection(COLLECTION_NAME);
+        ApiFuture<DocumentSnapshot> apiFuture
+                = collectionReference.document(memberId).get();
+        DocumentSnapshot snapshot = apiFuture.get();
+        Bookmark bookmark = null;
+
+        if (snapshot.exists()) {
+            bookmark = snapshot.toObject(Bookmark.class);
+            List<String> boardId = bookmark.getRoommateBoardId();
+
+            for(String id : boardId) {
+                ApiFuture<DocumentSnapshot> boardApi
+                        = collectionReference.document(id).get();
+                DocumentSnapshot documentSnapshot = boardApi.get();
+                list.add(documentSnapshot.toObject(RoommateBoard.class));
+            }
+            list.sort(Comparator.comparing(RoommateBoard::getCreated).reversed());
+            return list;
+        } else return null;
+    }
+
+    // 내가 북마크 한 글인지 확인 - 프로젝트
+    public boolean isBookmarkedP(String memberId, String boardId) throws Exception {
+        DocumentSnapshot snapshot
+                = firestore.collection(COLLECTION_NAME).document(memberId).get().get();
+        Bookmark bookmark = null;
+
+        if (snapshot.exists()) {
+            bookmark = snapshot.toObject(Bookmark.class);
+            if (bookmark.getProjectBoardId() != null && bookmark.getProjectBoardId().contains(boardId)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 내가 북마크 한 글인지 확인 - 프로젝트
+    public boolean isBookmarkedR(String memberId, String boardId) throws Exception {
+        DocumentSnapshot snapshot
+                = firestore.collection(COLLECTION_NAME).document(memberId).get().get();
+        Bookmark bookmark = null;
+
+        if (snapshot.exists()) {
+            bookmark = snapshot.toObject(Bookmark.class);
+            if ((bookmark.getRoommateBoardId() != null && bookmark.getRoommateBoardId().contains(boardId))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String removeProjectBookmark(String memberId, String boardId) throws Exception {
+        DocumentReference documentReference
+                = firestore.collection(COLLECTION_NAME).document(memberId);
+        DocumentSnapshot snapshot
+                = documentReference.get().get();
+        Bookmark bookmark = snapshot.toObject(Bookmark.class);
+        bookmark.getProjectBoardId().remove(boardId);
+        documentReference.set(bookmark);
+        return "remove";
+    }
+
+    public String removeRoommateBookmark(String memberId, String boardId) throws Exception {
+        DocumentReference documentReference
+                = firestore.collection(COLLECTION_NAME).document(memberId);
+        DocumentSnapshot snapshot
+                = documentReference.get().get();
+        Bookmark bookmark = snapshot.toObject(Bookmark.class);
+        bookmark.getRoommateBoardId().remove(boardId);
+        documentReference.set(bookmark);
+        return "remove";
+    }
+}
