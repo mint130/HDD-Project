6daf63c3e62d205b7fbf4b0dd0585dd8d7599e24
ooing-0720 <ooing-0720@naver.com>
2023-11-04 13:57:11 +0900

    11-04 마이페이지 및 북마크

--- src/main/java/com/HDD/recruitment/service/FileService.java
+++ src/main/java/com/HDD/recruitment/service/FileService.java
@@ -1,16 +1,19 @@
 package com.HDD.recruitment.service;
 
+import com.HDD.common.CustomMultipartFile;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Bucket;
 import com.google.firebase.cloud.StorageClient;
+import org.apache.commons.fileupload.DefaultFileItem;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
 
 @Service
 public class FileService {
@@ -38,10 +41,27 @@ public class FileService {
         blob.delete();
     }
 
-    // 되는지 모름
+    // 이미지 이름 -> 스토리지 URL 변환
     public String getImageUrl(String id) {
         Bucket bucket = StorageClient.getInstance().bucket(firebaseBucket);
         Blob blob = bucket.get(id.toString());
         return blob.getMediaLink();
     }
+
+    // 이미지 url -> MultipartFile 변환
+    public MultipartFile getFile(String id) throws  IOException{
+        String url = getImageUrl(id);
+        File file = new File(url, id);
+       // FileItem fileItem = new DiskFileItem("originFile", Files.probeContentType(file.toPath()), false, file.getName(), (int) file.length(), file.getParentFile());
+//        FileItem fileItem = new DefaultFileItem(file.getName(), Files.probeContentType(file.toPath()), false, file.getName(), (int) file.length(), file);
+        try {
+            InputStream input = new FileInputStream(file);
+//            OutputStream os = fileItem.getOutputStream();
+        } catch (IOException e) {
+            System.out.println("안됨");
+        }
+        MultipartFile multipartFile = new CustomMultipartFile(Files.readAllBytes(file.toPath()), id, id, "MultipartFile", file.length());
+        return multipartFile;
+    }
+
 }
